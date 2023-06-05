78f5be3dd117376a40985d67c3510ceb0dc332b7
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-05 23:24:01 +0300

    zadanie1.jl

--- zadanie1.jl
+++ zadanie1.jl
@@ -7,16 +7,6 @@ function mark_kross(r::Robot)
     end
     putmarker!(r)
 end
-putmarkers!(r::Robot,side::HorizonSide) = 
-while isborder(r,side)==false 
-    move!(r,side)
-    putmarker!(r)
-end
-move_by_markers(r::Robot,side::HorizonSide) = 
-while ismarker(r)==true 
-    move!(r,side) 
-end
-inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4)) 
 function gocentre(r::Robot)
     for i in (1:7)
     move!(r,HorizonSide(3))
