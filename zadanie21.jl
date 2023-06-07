727636de014d827165f3a119b8287e1ed1ca6684
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 09:55:44 +0300

    Update zadanie21.jl

--- zadanie21.jl
+++ zadanie21.jl
@@ -5,12 +5,8 @@ function double_dist!(robot,side)
         for _ in 1:2
             if !isborder(robot,inverse(side))
                 move!(robot,inverse(side))
- 
-            else
-                return false
             end
         end
     end
-    return true
 end
 inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 2, 4))
