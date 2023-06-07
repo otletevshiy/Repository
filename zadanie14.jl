897977db142b5892b7790dc96f73db6b5a93386d
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 10:00:13 +0300

    zadanie14.jl

--- zadanie14.jl
+++ zadanie14.jl
@@ -3,23 +3,20 @@ using HorizonSideRobots
 function try_move!(robot, side)
     if isborder(robot, side)
         return false
-    else 
         move!(robot, side)
-        return true
     end
 end
-
 
 function along!(robot, side)
     while try_move!(robot, side) 
     end
 end
 
-function snake!( robot, (move_side, next_row_side)::NTuple{2,HorizonSide} = (Nord, Ost))
-    along!(robot, move_side)
-    while try_move!(robot, next_row_side)
-        move_side = inverse(move_side)
-        along!(robot, move_side)
+function snake!(robot, side)
+    along!(robot, side)
+    while try_move!(robot, side)
+        side = inverse(side)
+        along!(robot, side)
     end
 end
 
