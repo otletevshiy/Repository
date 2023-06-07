10ab73aa5b06adc4f1739bcb69a1c54c97021e76
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 17:34:14 +0300

    zadanie4.jl

--- zadanie4.jl
+++ zadanie4.jl
@@ -1,7 +1,7 @@
 using HorizonSideRobots
 r=Robot(15,15; animate=true)
-function cross_move(robot, side_x, side_y)
-    num_step = 0
+function cross_move(robot, side_x, side_y)     #Функция перемещает робота по кресту начиная с текущего положения, если робот достиг границы поля, то направление меняется на противоположное, и движется покуда не достигнет начальной позиции
+    num_step = 0  
     while !isborder(robot, side_x) && !isborder(robot, side_y)
         move!(robot, side_x)
         move!(robot, side_y)
@@ -28,7 +28,7 @@ function cross_move(robot, side_x, side_y)
     end
 end
 
-function plus!(robot)
+function plus!(robot)   #Функция перемещает робота по всем возможным крестам на поле, вызывая функцию cross_move для каждого креста, после чего ставит маркер
     for side_x in (Nord, Sud)
         for side_y in (Ost, West)
             cross_move(robot, side_x, side_y)
