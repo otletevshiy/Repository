389a0bdb0494f8c4f4d9d8411fec22fa6033963c
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 17:43:49 +0300

    zadanie7.jl

--- zadanie7.jl
+++ zadanie7.jl
@@ -1,4 +1,4 @@
-function iceout!(robot,side)
+function iceout!(robot,side)   #Функция перемещает робота на n клеток в направлении side_for_move
     side1 = side
     side_for_move = right(side1)
     n = 0
@@ -9,12 +9,12 @@ function iceout!(robot,side)
     end
 end
 
-function along!(robot,side,n_steps)
+function along!(robot,side,n_steps)   #Функция перемещает робота на указанное количество шагов вдоль указанной стороны
     for _ in 1:n_steps
         move!(robot,side)
     end
 end
 
 
-right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 1 , 4)) 
-inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 2 , 4))
+right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 3 , 4))   #Функция меняет сторону на другую сторону которая находится справа по часовой стрелке
+inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 2 , 4))   #Функция меняет сторону на противоположную
