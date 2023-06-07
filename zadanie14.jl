260282c6ce50d269358c36ff4619ae5597db73f7
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 18:10:00 +0300

    zadanie14.jl

--- zadanie14.jl
+++ zadanie14.jl
@@ -1,18 +1,18 @@
 using HorizonSideRobots
 
-function try_move!(robot, side)
+function try_move!(robot, side)   #Функция возвращает значение false, если робот стоит на границе, в противном случае робот перемещается в заданном направлении
     if isborder(robot, side)
         return false
         move!(robot, side)
     end
 end
 
-function along!(robot, side)
+function along!(robot, side)    #Функия вызывает функцию try_move! до тех пор она не вернет значение false
     while try_move!(robot, side) 
     end
 end
 
-function snake!(robot, side)
+function snake!(robot, side)   #
     along!(robot, side)
     while try_move!(robot, side)
         side = inverse(side)
@@ -20,4 +20,4 @@ function snake!(robot, side)
     end
 end
 
-inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
+inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)   #Функция меняет направление на противоположное
