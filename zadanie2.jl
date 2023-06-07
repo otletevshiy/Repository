8090d4f17b52d6d8a875917581ccfaf9f2de984f
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 17:28:06 +0300

    zadanie2.jl

--- zadanie2.jl
+++ zadanie2.jl
@@ -1,6 +1,6 @@
 using HorizonSideRobots
 r=Robot(15,15; animate=true)
-function perimeter!(robot)
+function perimeter!(robot)  #Перемещает робота по периметру, попутня ставя маркеры на каждой стороне
     for side in (Ost, Nord, West, Sud)
         while !isborder(robot, side)
             move!(robot, side)
