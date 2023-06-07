f1d1924f9060f47cb262083fbc946dc82c3013db
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 18:20:46 +0300

    zadanie20.jl

--- zadanie20.jl
+++ zadanie20.jl
@@ -1,5 +1,5 @@
 using HorizonSideRobots
-function move_next!(robot, side)
+function move_next!(robot, side)   #Функция проверяет есть ли клетка, раположенная в заданном направлении, если нет то робот перемещается перемещается в заданном направлении. если клетка в направлении является границей поля, то робот сначала перемещается на одну клетку, затем вызывает сама себя, но с противоположной стороной, затем перемещается на одну клетку в указанном направлении
     if !isborder(robot, side)
         move!(robot, side)
     else
@@ -9,5 +9,5 @@ function move_next!(robot, side)
     end
 end
 
-right(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)
-left(side::HorizonSide) = HorizonSide((Int(side) +3)% 4)
+right(side::HorizonSide) = HorizonSide((Int(side) +3)% 4)   #Функция меняет направление на направление справа по часовой стрелке
+left(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)    #Функция меняет направление на направление слева против часовой стрелке
