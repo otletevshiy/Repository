c67a10209da21662ced1d3252a30a799c2a20b97
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-06-07 18:23:03 +0300

    zadanie18.jl

--- zadanie18.jl
+++ zadanie18.jl
@@ -1,5 +1,5 @@
 using HorizonSideRobots
-function move_recursive!(robot::Robot, side::HorizonSide)
+function move_recursive!(robot::Robot, side::HorizonSide)  #Функция проверяет наличие границы поля и перемещает его в заданном направлении,после чего функция вызывает сама себя
     if !isborder(robot, side)
         move!(robot, side)
         move_recursive!(robot, side)
