489fb3f6eb980cbf30c72e68470b339427d07c08
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-10-17 23:07:21 +0300

    zadanie3.jl

--- zadanie3.jl
+++ zadanie3.jl
@@ -1,6 +1,7 @@
 using HorizonSideRobots
 r=Robot(15,15; animate=true)
-function full!(robot)  #Функция полностью закрашивает поле
+function full!(robot) #Фунция полностью закрашивает поле
+    corner!(robot)
     putmarker!(robot)
     side = Ost;
     while !isborder(robot, side)
@@ -13,5 +14,16 @@ function full!(robot)  #Функция полностью закрашивает
         end
     end
 end
-inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)   #Функция меняет сторону на противоположную
+
+function corner!(robot) #Перемещает робота в левый нижний угол
+    while !isborder(robot, Sud)
+        move!(robot, Sud)
+    end
+    while !isborder(robot, West)
+        move!(robot, West)
+    end
+end
+
+inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4) #Функция возвращает противоположную горизонтальную сторону относительно указанной стороны
 full!(r)
+corner!(r)
